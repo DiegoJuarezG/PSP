0d97dcf44bac46ba6b157481806491a8dd2aab1b
DiegoJuarezG <114983876+DiegoJuarezG@users.noreply.github.com>
2022-10-18 10:17:41 +0200

    Add files via upload

---
+++ UT1/signal-4.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/* gestión de señales en proceso padre*/
+void manejador( int numsenal )
+{
+    printf("Hijo recibe señal número..%d\n", numsenal);
+ 
+}
+
+void manejadorP( int numsenal )
+{
+    printf("Padre recibe señal número..%d\n", numsenal);
+ 
+}
+
+int main()
+{
+    int pid_hijo;  
+    pid_hijo = fork(); //creamos hijo   
+    
+    switch(pid_hijo)
+    {
+        case -1:
+            printf( "Error al crear el proceso hijo...\n");
+            exit( -1 );        
+        case 0:   //HIJO     	         
+            signal(SIGUSR1, manejador); //Función manejadora de la señal
+            pause();
+            kill(getppid(),SIGILL);
+            break;    
+        default: //PADRE envia 1 señal
+            signal(SIGILL, manejadorP); //Función manejadora de la señal
+            printf("Soy proceso padre, mi id: %d\n",getpid());
+            sleep(1);
+            printf("Voy a enviar la señal al hijo, su id: %d\n",pid_hijo);
+            kill(pid_hijo, SIGUSR1);//ENVIA SEÑAL AL HIJO 
+            sleep(1);
+            
+            break;
+    } 
+    return 0;
+}
